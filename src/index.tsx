8939d562b2420c394e7ce2f55c8d70335aa103d9
Willaiem <vvwill4u@gmail.com>
2022-04-06 18:38:44 +0200

    Init & fixed the import paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { AppProviders } from 'providers/AppProviders';
+import { AppProviders } from './providers/AppProviders';
 
 import { App } from './app/App';
 import * as serviceWorker from './serviceWorker';
