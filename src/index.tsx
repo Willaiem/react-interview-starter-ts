a94471889e652cad4700d60c36fd1e13d247366e
Willaiem <vvwill4u@gmail.com>
2022-04-06 18:41:49 +0200

    Removed the service worker

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import { AppProviders } from './providers/AppProviders';
 
 import { App } from './app/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <AppProviders>
@@ -13,7 +12,3 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
